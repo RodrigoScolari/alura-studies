878b9abd081bf708129f40ff226d3085d8cc0d09
Rodrigo A Scolari <49165601+RodrigoScolari@users.noreply.github.com>
2022-09-05 17:22:37 -0300

    aula05 - State

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import style from './Botao.module.scss';
 
-class Botao extends React.Component <{children?: React.ReactNode}> {
-  render() { 
+class Botao extends React.Component<{
+  children?: React.ReactNode;
+  type?: 'button' | 'submit' | 'reset' | undefined;
+}> {
+  render() {
+    const { type = 'button' } = this.props;
     return (
-      <button className={style.botao}>
+      <button type={type} className={style.botao}>
         {this.props.children}
       </button>
-    )
+    );
   }
 }
 
--- src/components/pages/App.tsx
+++ src/components/pages/App.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { ITarefa } from '../../types/tarefa';
 import Cronometro from '../Cronometro';
 import Formulario from '../Formulario';
 import Lista from '../Lista';
 import style from './App.module.scss';
 
 function App() {
+  const [tarefas, setTarefas] = useState<ITarefa[]>([]);
+
   return (
     <div className={style.AppStyle}>
-      <Formulario />
-      <Lista />
+      <Formulario setTarefas={setTarefas} />
+      <Lista tarefas={tarefas} />
       <Cronometro />
     </div>
   );

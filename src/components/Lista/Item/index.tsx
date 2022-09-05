39c994df6cd5261cb4e2e7a7c63ad3f147745d12
Rodrigo A Scolari <49165601+RodrigoScolari@users.noreply.github.com>
2022-09-05 18:11:54 -0300

    aula06 - Selecionando um item

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -1,16 +1,31 @@
 import { ITarefa } from '../../../types/tarefa';
-import style from '../Lista.module.scss';
+import style from './Item.module.scss';
+
+interface Props extends ITarefa {
+  selecionaTarefa: (tarefaSelecionada: ITarefa) => void;
+}
 
 export default function Item({
   tarefa,
   tempo,
   selecionado,
   completado,
-  id
-}: ITarefa) {
-  console.log('item atual: ', { tarefa, tempo, selecionado, completado, id });
+  id,
+  selecionaTarefa
+}: Props) {
   return (
-    <li className={style.item}>
+    <li
+      className={`${style.item} ${selecionado ? style.itemSelecionado : ''}`}
+      onClick={() =>
+        selecionaTarefa({
+          tarefa,
+          tempo,
+          selecionado,
+          completado,
+          id
+        })
+      }
+    >
       <h3> {tarefa} </h3>
       <span> {tempo} </span>
     </li>
